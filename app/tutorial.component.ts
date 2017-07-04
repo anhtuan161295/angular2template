ee1904fb0b60159cb3bdba17157675219903c9e4
QQ <anhtuan161295@gmail.com>
2017-07-04 22:08:25 +0700

    test

---
+++ app/tutorial.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'my-tutorial',
+    template:`<h2>This TEDU Angular2 Tutorial component</h2>
+              <h4>Sub Component</h4>
+    `
+    // styles: ['h2 {color:green}']
+})
+export class TutorialComponent{
+
+}
