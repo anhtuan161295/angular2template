a0430e10b6493b4c92bfcddb3d562c6b7643edf8
QQ <anhtuan161295@gmail.com>
2017-07-05 18:38:46 +0700

    lesson15

--- app/app.component.js
+++ app/app.component.js
@@ -9,18 +9,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        // interpolation
-        this.title = "Hello TEDU Channel";
-        this.today = Date.now();
-        this.percentNumber = 1.3495;
-        this.e = 2.718281828459045;
-        this.object = { foo: 'bar', baz: 'qux', nested: { xyz: 3, numbers: [1, 2, 3, 4, 5] } };
-        this.collection = ['a', 'b', 'c', 'd'];
     }
+    AppComponent.prototype.onSubmit = function (value) {
+        console.log(value);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1> {{title | lowercase }}!</h1>\n  <p>Date today: {{today | date:'shortDate' }} </p>\n  <p>Percent: {{percentNumber | percent }}</p>\n  <p>e (3.1-5): {{e | number:'3.1-5' }}  </p>\n  <pre>{{object | json }}</pre>\n  <p>2 power 10: {{2 | exponentialStrength: 10 }}</p>\n  <ul>\n    <li *ngFor=\"let i of collection | slice:1:3 \" >{{i}}</li>\n  </ul>\n  <my-tutorial  ></my-tutorial>\n  ",
+            templateUrl: '/app/app.component.html'
         })
     ], AppComponent);
     return AppComponent;
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import {TutorialComponent} from './tutorial.component';
 import {FormsModule} from '@angular/forms';
-import {ExponentialStrenthPipe} from './exponential-strength.pipe.js'
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,TutorialComponent, ExponentialStrenthPipe],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
