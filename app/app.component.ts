8b56f769b02ae597bb60e4380eca91064292812f
QQ <anhtuan161295@gmail.com>
2017-07-05 16:44:30 +0700

    lesson13

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TutorialComponent } from './tutorial.component'
 @Component({
   selector: 'my-app',
   template: `
   <h1>Helle {{title}}!</h1>
-  <my-tutorial></my-tutorial>
+  <input type="text" #textName (keyup)="0" />
+  <p>Agree number: {{agree}}. Disagree: {{disagree}} </p>
+  <button (click)="changeName()" >Change name</button>
+  <my-tutorial *ngFor="let person of names" [name]="person" (onVote)="parentVote($event)" ></my-tutorial>
   `,
 
 })
 export class AppComponent {
   // interpolation
   public title = "TEDU Channel";
+  public agree: number = 0;
+  public disagree: number = 0;
+  public names = ['Mr A', 'Mr B', 'Mr C', 'Mr D'];
 
+  @ViewChild(TutorialComponent)
+  private tutorialComponent: TutorialComponent;
+
+  parentVote(agree: boolean) {
+    if (agree) this.agree++;
+    else this.disagree++;
+  }
+  changeName(){
+    this.tutorialComponent.setName('Change name in Parent');
+  }
 }
--- app/tutorial.component.ts
+++ app/tutorial.component.ts
@@ -1,32 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
     template: `
     <h2> {{title}} </h2>
-    <p [ngClass]="{classOne:cone, classTwo:ctwo}" >This ngClass apply style</p>
-    <button (click)="toggle()">Toggle</button>
-    <p [ngStyle]="{'font-style':style, 'font-size':size}" >This paragraph will be apply to ngStyle</p>
-    `,
-    styles: [
-        `
-        .classOne{
-            color:white;
-        }
-        .classTwo{
-            background-color:black;
-        }
-        `
-    ]
+    <p>Child component: {{name}}</p>
+
+    <button [disabled]="voted" (click)="vote(true)" >Agree</button>
+    <button [disabled]="voted" (click)="vote(false)" >Disagree</button>
+    Result: {{voted}}
+    `
+
 })
 export class TutorialComponent {
     public title = "This TEDU Angular2 Tutorial component";
-    public cone = true;
-    public ctwo = true;
-    public style = "italic";
-    public size = "30px";
-    toggle() {
-        this.cone = !this.cone;
-        this.ctwo = !this.ctwo;
+    @Input() name: string;
+    @Output() onVote = new EventEmitter<boolean>();
+
+    public voted: boolean = false;
+
+    setName(name: string) {
+        this.name = name;
+    }
+
+    vote(agree: boolean) {
+        this.voted = false;
+        this.onVote.emit(agree);
     }
 }
